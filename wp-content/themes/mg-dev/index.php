965c81c441df66b6c3a3f5cf322d3f1e5a50d43e
Michael Gale <michael@michaelgale.dev>
2020-12-29 09:07:01 +1100

    Add WP loop to dev env

--- wp-content/themes/mg-dev/index.php
+++ wp-content/themes/mg-dev/index.php
@@ -1,8 +1,20 @@
 <?
-
 /**
  * WP HEADLESS 💕
- * Prevent public access to theme routes
+ * Output standard WordPress loop (development)
+ * Prevent public access to theme routes (production)
  */
-// header('Location: https://michaelgale.dev');
-// exit();
+if ( wp_get_environment_type() === 'local' ) {
+  get_header();
+  if (have_posts()) :
+    while (have_posts()) :
+      the_post();
+      the_content();
+    endwhile;
+  endif;
+  get_sidebar();
+  get_footer(); 
+} else {
+  header('Location: https://michaelgale.dev');
+  exit();
+}
